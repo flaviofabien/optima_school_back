dc0b786c742fde857d5e6281974968b11dd8d8bc
flaviofabien <fabien4240flavio@gmail.com>
2025-09-11 11:09:33 +0300

    with front

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,32 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.ADMIN_MYSQL_BD,
-  process.env.ADMIN_MYSQL_ROOT,
-  process.env.ADMIN_MYSQL_PASSWORD,
-  {
-    host: process.env.ADMIN_MYSQL_HOST,
-    dialect: "mariadb",
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
     logging: false,
     timezone: "+02:00",
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.ADMIN_MYSQL_BD,
+    process.env.ADMIN_MYSQL_ROOT,
+    process.env.ADMIN_MYSQL_PASSWORD,
+    {
+      host: process.env.ADMIN_MYSQL_HOST,
+      dialect: "mariadb",
+      logging: false,
+      timezone: "+02:00",
+    }
+  );
+}
 
 module.exports = sequelize;
